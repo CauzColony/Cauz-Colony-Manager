e0d71e9acf63b5d547b61b1d3cb32645ed4e9c92
Jonathan Andersen <jon@littlemama.hsd1.mi.comcast.net>
2014-11-09 20:10:52 -0500

    working on models

---
+++ models/Charity.js
@@ -0,0 +1,68 @@
+var keystone = require('keystone'),
+	Types = keystone.Field.Types;
+
+/**
+ * User Model
+ * ==========
+ */
+
+var Charity = new keystone.List('Charity');
+
+Charity.add({
+	name: { type: Types.Text, required: true, index: true },
+  logoLarge: { type: Types.CloudinaryImage, autoCleanup : true, note: 'sized to exactly 600x280 on a white background'},
+  logoSmall: { type: Types.CloudinaryImage, autoCleanup : true, note: 'sized to exactly 300x140 on a white background'}
+});
+
+
+/**
+ * Registration
+ */
+
+Charity.defaultColumns = 'name, active, public';
+Charity.register();
+
+
+
+/*
+{
+  title: 'Powerbeats',
+  offer: {
+    link: 'http://www.beatsbydre.com/earphones/powerbeats/beats-powerbeats.html',
+    text: 'You’ve also earned<br>this exclusive offer<br>from Powerbeats <span>&gt;</span>',
+    image: 'assets/images/powerbeats@2x.png',
+    style: 'margin: 70px 0 0 115px; display:block; line-height:1.2; font-size: 16px;'
+  },
+  charityId: 0,
+  charityName: 'SF-MARIN FOOD BANK',
+  id: '0',
+  image: 'assets/images/logos/sf-martin@2x.png',
+  steps: [
+    {
+      type: 'video',
+      copy: 'Powerbeats. Earbuds designed for athletes by LeBron James and Dr. Dre. Learn more below. Press play.',
+      id: '7qraNrqA2pw'
+    },
+    {
+      type: 'rating',
+      text: '<span>MADE FOR ATHLETES.</span><br>Flexible earclips are designed to<br>secure Powerbeats earphones in<br>your ears no matter how rigorous<br>your workout.',
+      options: [ 1, 2, 3, 4]
+    },
+    {
+      type: 'rating',
+      text: '<span>CLEARER SOUND. DEEPER BASS.</span><br>Powerbeats earphones are the<br>only Beats by Dr. Dre earbuds<br>that come with two speakers<br>inside each bud. That means you<br>get crystal clear highs and deep,<br>rumbling lows',
+      options: [ 1, 2, 3, 4]
+    },
+    {
+      type: 'rating',
+      text: '<span>SAFETY FIRST.</span><br>Powerbeats earphones are<br>specially designed to pump<br>clear bass at any volume while<br>letting in ambient noise – making<br>sure athletes stay safe while<br>running on the road.',
+      options: [ 1, 2, 3, 4]
+    },
+    {
+      type: 'rating',
+      text: '<span>REMOTE CONTROL CORD.</span><br>You can adjust your music to<br>find your power song right from<br>the cord. No need to fumble<br>with your MP3 player during<br>your workout.',
+      options: [ 1, 2, 3, 4]
+    }
+  ]
+}
+*/
